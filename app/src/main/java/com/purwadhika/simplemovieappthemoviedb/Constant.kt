5d41ec05e3743837f17cc2336ae25ef5a59bd637
Viki Andrianto <vikiandrianto88@gmail.com>
2020-12-30 07:28:28 +0700

    remove api key

--- app/src/main/java/com/purwadhika/simplemovieappthemoviedb/Constant.kt
+++ app/src/main/java/com/purwadhika/simplemovieappthemoviedb/Constant.kt
@@ -3,6 +3,7 @@ package com.purwadhika.simplemovieappthemoviedb
 class Constant {
     companion object{
         const val BASE_URL = "https://api.themoviedb.org/3/"
-        const val API_KEY = "06d7f99d5aa3b1f85f3910c0b52b8e5f"
+        // Todo : Put your APIKey TMDB here
+        const val API_KEY = ""
     }
 }
